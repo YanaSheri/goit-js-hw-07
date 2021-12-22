d74efb6cc25db11eb4542dc5d847134b3158177b
YanaSheri <yana.shereheda@gmail.com>
2021-12-22 15:40:54 +0200

    Update 2nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const listGallery = document.querySelector('.gallery');
+
+//<a class="gallery__item" href="large-image.jpg">
+ // <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+//</a>
+
+const createItems = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<li>
+                    <a class="gallery__item" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            alt="${description}"
+                        />
+                    </a>
+                </li>`
+    }).join("");
+
+listGallery.insertAdjacentHTML("afterbegin", createItems);
+const cb = (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return false;
+    const sourceImg = event.target.dataset.source;
+    console.log(sourceImg);
+};
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', animationSpeed: "250ms" });
+lightbox.close();
+
+listGallery.addEventListener('click', cb);
+
+
 console.log(galleryItems);
+
+
